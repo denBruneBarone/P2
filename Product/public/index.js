03ad4772550e21dca058c97bfd9b61a962ce45c4
denBruneBarone <runeberhardt@gmail.com>
2022-03-26 13:58:51 +0100

    begyndt på auth af github

--- Product/public/index.js
+++ Product/public/index.js
@@ -1,3 +1,14 @@
-// hej :)
-// hej igen
-// hej igen igen
+// Selectors
+const githubButton = document.querySelector('.githubButton');
+
+// Event listeners
+githubButton.addEventListener('click', authorizeGithub);
+
+// Functions
+function authorizeGithub(event) {
+   event.preventDefault();
+   console.log("Du har trykket på GitHub-knappen din noob");
+
+   // Gør så vi kun kan trykke på knappen igen
+   githubButton.disabled = true;
+}
