2e4e1583c1f3e0d7cef4bc38abaaf2de75e989c6
denBruneBarone <runeberhardt@gmail.com>
2022-03-26 10:51:22 +0100

    oprettet express server

---
+++ Product/server.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+const express = require("express");
+const app = express();
+app.get("/", (req, res) => { res.sendFile(__dirname + "/index.html"); });
+// cookie-test
+/* app.get("/test-cookie", (req, res) => { res.sendFile(dirname + "/cookie_test.html") }); */
+app.post("/", async (req, res) => {
+   const buffers = [];
+   for await (const chunk of req) { buffers.push(chunk) }
+   const data = Buffer.concat(buffers).toString();
+   console.log("The user is interested in " + JSON.parse(data))
+   fs.appendFile('message.txt', JSON.parse(data) + "\n", () => { })
+})
+app.listen(3000, () => console.log("Server is running on http://localhost:3000"));
