c6752cc202064045a48f78a15090addc79403f36
matthias <mattja21@student.aau.dk>
2022-03-31 22:42:16 +0200

    green border when auth api, design fixes, time interval

--- Product/public/overview.js
+++ Product/public/overview.js
@@ -1,31 +1,35 @@
-function timeInterval(){
-    var dateStringStart = document.getElementByCl(startTime).valueAsNumber
-    var dateStringStart = document.getElementById(startTime).valueAsNumber
+function timeInterval() {
+  var dateStringStart = document.getElementById(startTime).value;
+  var dateStringEnd = document.getElementById(endTime).value;
 
-    console.log("dateStringStart")
-    if(dateStringEnd < dateStringStart){
-        alert("End date cannot be less than the start date!")
-        return false
-    }
+    console.log(dateStringStart)
+    console.log(dateStringEnd)
 
-    //Bruges til displayData function
+  if (dateStringEnd.getTime() < dateStringStart.getTime()) {
+    alert("End date cannot be less than the start date!");
+    return false;
+  }
+  return true;
+  //Bruges til displayData function
 }
 
-function authApi(){
-    //Mulighed for at authenticate APIs, hvis man ikke havde gjort det før
-    //Toggle buttons til ikke authenticated er disabled indtil da
+function authApi() {
+  //Mulighed for at authenticate APIs, hvis man ikke havde gjort det før
+  //Toggle buttons til ikke authenticated er disabled indtil da
 }
 
-function toggleApi(btn_id){
+function toggleApi(btn_id) {
+  if (document.getElementById(btn_id).value == "enabled") {
+    document.getElementById(btn_id).value = "disabled";
+    document.getElementById(btn_id).style = "border-color: red";
+    //Add function til at stoppe afsending af data i box
+  } else if (document.getElementById(btn_id).value == "disabled") {
+    document.getElementById(btn_id).value = "enabled";
+    document.getElementById(btn_id).style = "border-color: #27af49";
+    //Add function til at sende data i box
+  }
+}
 
-    if(document.getElementById(btn_id).value == "enabled"){
-        document.getElementById(btn_id).value = "disabled"
-        document.getElementById(btn_id).style = 'border-color: red'
-        //Add function til at stoppe afsending af data i box
-    }
-    else if (document.getElementById(btn_id).value == "disabled"){
-        document.getElementById(btn_id).value = "enabled"
-        document.getElementById(btn_id).style = 'border-color: #27af49'
-        //Add function til at sende data i box
-    }
+function onLoadOverview() {
+    //timeInterval()
 }
