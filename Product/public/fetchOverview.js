7f6b74d07b764ae668655b82e36b1a7004cd1311
matthias <mattja21@student.aau.dk>
2022-03-31 13:27:33 +0200

    overview, time interval, css, toggle, submit

---
+++ Product/public/fetchOverview.js
@@ -0,0 +1,7 @@
+function fetchData(){
+
+}
+
+function displayData(){
+    //Det fetchede data bliver indsat med et time interval fra overview.js, ind i en boks som text.
+}
---
+++ Product/public/overview.js
@@ -0,0 +1,30 @@
+function timeInterval(){
+    var dateStringStart = document.getElementById("timeInputStart").value
+    var dateStringEnd = document.getElementById("timeInputEnd").value
+
+    if(dateStringEnd < dateStringStart){
+        alert("End date cannot be less than the start date!")
+        return false
+    }
+
+    //Bruges til displayData function
+}
+
+function authApi(){
+    //Mulighed for at authenticate APIs, hvis man ikke havde gjort det før
+    //Toggle buttons til ikke authenticated er disabled indtil da
+}
+
+function toggleApi(btn_id){
+
+    if(document.getElementById(btn_id).value == "enabled"){
+        document.getElementById(btn_id).value = "disabled"
+        document.getElementById(btn_id).style = 'border-color: red'
+        //Add function til at stoppe afsending af data i box
+    }
+    else if (document.getElementById(btn_id).value == "disabled"){
+        document.getElementById(btn_id).value = "enabled"
+        document.getElementById(btn_id).style = 'border-color: green'
+        //Add function til at sende data i box
+    }
+}
