eb786956306c0160e221266972d538d372015316
Moritz <m.h98@gmx.net>
2022-03-31 14:29:07 +0200

    moritz/minor-fixes

--- Product/public/discordAuthentication.js
+++ Product/public/discordAuthentication.js
@@ -1,15 +1,11 @@
-
 // send code to server to send a post request, recieve the token back
-let code = window.location.search.substring(window.location.search.indexOf("=") + 1)
-console.log(typeof code)
-
 fetch("http://localhost:3000/discord-code", {
     method: "POST",
     headers: { 
         'Content-Type': 'application/json'
     },
     body: JSON.stringify({
-        code: code
+        code: window.location.search.substring(window.location.search.indexOf("=") + 1)
     })
 }).then(r => {
     r.json().then(data => {
