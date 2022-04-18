32857646d5e24df5bbdb85178d73b7bf72576cfb
AAUemi <emiped21@student.aau.dk>
2022-04-18 09:28:09 +0200

    Getting discord guild list:)

--- Product/public/retrieveFrom.js
+++ Product/public/retrieveFrom.js
@@ -79,7 +79,7 @@ async function createLists() {
     getTrelloBoards();
   }
   if (Tokens.discord) {
-
+    getDiscordGuilds();
   }
 }
 
@@ -134,24 +134,30 @@ async function getTrelloBoards() {
   });
 }
 
+/* Retrieves an authorized discord users server/guild list */
 async function getDiscordGuilds(){
-  const Discord = require("discord.js");
-  const client = new Discord.Client();
+  let discordGuilds = await fetch(`https://discord.com/api/users/@me/guilds`, {
+    method: "GET",
+    headers: {
+      "Authorization": "Bearer " + window.sessionStorage.getItem("discord-token"),
+    }
+  }
+  );
 
-  const guildRes = await fetch(
-    `https://discordapp.com/api/users/@me/guilds`,
-    {
-      headers:{
-        Authorization: 'Bearer' + DISCORD_SECRET
-      }
-    })
-    const guilds = await guildRes.json()
-    res.send(guilds)
-    console.log(guilds)
+  let discordGuildList = await discordGuilds.json();
 
-  client.on("ready", () => {
-    const Guilds = client.guilds.cache.map(guild => guild.id);
-    console.log(Guilds);
-  });
-  client.login(process.env.DISCORD_SECRET)
+  /* Overvej måske at tilføje discord server ikonet udenfor navnet! Vi kan anskaffe det bare ved "icon" for at gøre der lidt pænt */
+  /*let discordGuildForm = document.getElementById("discord-submit")*/
+  for (i of discordGuildList) {
+    /*discordGuildForm.innerHTML =
+      `<input class="discordGuilds" type="checkbox" id="${i.id}" value="${i.name}"><label for="${i.id}">${i.name}</label><br>` +
+      discordGuildForm.innerHTML;*/
+  }
 }
+
+async function getDiscordChannels(){
+
+
+
+}
+
